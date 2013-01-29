70ffe74138c9165c7b7483454672208d75d1168f
Mikkel-L <lep.mik@gmail.com>
2013-01-29 16:42:54 +0100

    endaengang

--- testmodule.py
+++ testmodule.py
@@ -8,9 +8,9 @@ Description:
 __author__ = 'Mikkel Lepperod'
 __email__ = 'lep.mik@gmail.com'
 
-#import numpy as np
-#from numpy import sin, cos, pi, exp
-#from mayavi import mlab
+import numpy as np
+from numpy import sin, cos, pi, exp
+from mayavi import mlab
 
 
 if __name__ == '__main__':
