e6d2932927a646e42a0489c89aa58a3198308f93
Mikkel-L <lep.mik@gmail.com>
2013-01-29 16:40:57 +0100

    blabal

---
+++ testmodule.py
@@ -0,0 +1,18 @@
+'''
+Short name:
+
+29. jan. 2013
+
+Description: 
+'''
+__author__ = 'Mikkel Lepperod'
+__email__ = 'lep.mik@gmail.com'
+
+#import numpy as np
+#from numpy import sin, cos, pi, exp
+#from mayavi import mlab
+
+
+if __name__ == '__main__':
+    pass
+    
